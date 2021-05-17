291a8a83ced4c97f7214bdd20af764d5101c0313
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-05-18 01:49:27 +0800

    dev: added stub for MainScreen

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default (props) => {
+  return (
+    <View></View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
