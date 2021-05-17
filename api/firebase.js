18ceecb87746581e81fec54bb7101cf0da25051e
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-05-18 01:47:34 +0800

    feat: added Firebase initialiser

---
+++ api/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID
+} from "@env";
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
+};
+
+const firebaseApp = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+export default firebaseApp;
