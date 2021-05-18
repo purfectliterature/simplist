940e40c458d6e5ba9331e8362fd39e69e54cbc65
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-05-18 13:03:38 +0800

    feat: added Firebase Authentication

---
+++ api/auth.js
@@ -0,0 +1,34 @@
+import firebase from "./firebase";
+
+const auth = firebase.auth();
+
+export const signIn = async ({ email, password }, onSuccess, onError) => {
+  try {
+    const { user } = await auth.signInWithEmailAndPassword(email, password);
+    return onSuccess(user);
+  } catch (error) {
+    return onError(error);
+  }
+}
+
+export const createAccount = async ({ name, email, password }, onSuccess, onError) => {
+  try {
+    const { user } = await auth.createUserWithEmailAndPassword(email, password);
+    if (user) {
+      await user.updateProfile({ displayName: name });
+      await user.sendEmailVerification();
+      return onSuccess(user);
+    }
+  } catch (error) {
+    return onError(error);
+  }
+}
+
+export const signOut = async (onSuccess, onError) => {
+  try {
+    await auth.signOut();
+    return onSuccess();
+  } catch (error) {
+    return onError(error);
+  }
+}
--- api/firebase.js
+++ api/firebase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import "firebase/auth";
 import {
   API_KEY,
   AUTH_DOMAIN,
