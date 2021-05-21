f0e988a862a8081f452708477f59713bd5b28580
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-05-22 03:47:53 +0800

    feat: integrated Firebase Realtime Database

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,42 +1,39 @@
-import React, { useState } from "react";
-import { StyleSheet, View, SectionList, Text, StatusBar, SafeAreaView, TextInput } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, View, SectionList, TextInput } from "react-native";
 import { Checkbox, IconButton, List, Menu } from "react-native-paper";
 import { CommonActions } from "@react-navigation/native";
 
 import Screen from "../components/Screen";
-import Brand from "../components/Brand";
 
 import * as Authentication from "../../api/auth";
-
-class Task {
-  constructor(subject, completed) {
-    this.subject = subject;
-    this.completed = completed;
-  }
-}
-
-const lists = [
-  {
-    title: "Pending",
-    data: [
-      new Task("Pizza", false),
-      new Task("Burger", false),
-      new Task("Risotto", false)
-    ]
-  },
-  {
-    title: "Completed",
-    data: [
-      new Task("French Fries", true),
-      new Task("Onion Rings", true),
-      new Task("Fried Shrimps", true)
-    ]
-  }
-]
+import * as Tasks from "../../api/tasks";
 
 export default ({ navigation }) => {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
   const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState({});
+  const [sectionedTasks, setSectionedTasks] = useState([]);
+  const [userId, setUserId] = useState(Authentication.getCurrentUserId());
+  const newTaskRef = useRef();
+
+  useEffect(() => {
+    return Tasks.subscribe(userId, setTasks);
+  }, []);
+
+  useEffect(() => {
+    if (tasks) {
+      const sortedTasks = [];
+
+      const tasksArray = Object.values(tasks);
+      const completedTasks = tasksArray.filter((task) => task.completed);
+      const pendingTasks = tasksArray.filter((task) => !task.completed);
+
+      if (pendingTasks.length > 0) sortedTasks.push({ title: "Pending", data: pendingTasks });
+      if (completedTasks.length > 0) sortedTasks.push({ title: "Completed", data: completedTasks });
+
+      setSectionedTasks(sortedTasks);
+    }
+  }, [tasks]);
 
   const handleLogout = () => {
     setIsMenuVisible(false);
@@ -50,6 +47,36 @@ export default ({ navigation }) => {
     );
   }
 
+  const handleCreateTask = () => {
+    const subject = newTask;
+    setNewTask("");
+    newTaskRef.current.clear();
+
+    return Tasks.createTask(
+      { userId, subject },
+      () => {},
+      console.error
+    );
+  }
+
+  const handleCompleteTask = (taskId) => Tasks.completeTask(
+    { userId: Authentication.getCurrentUserId(), taskId }, 
+    () => {}, 
+    console.error
+  );
+
+  const handleUncompleteTask = (taskId) => Tasks.uncompleteTask(
+    { userId: Authentication.getCurrentUserId(), taskId }, 
+    () => {}, 
+    console.error
+  );
+
+  const handleDeleteTask = (taskId) => Tasks.deleteTask(
+    { userId: Authentication.getCurrentUserId(), taskId },
+    () => {},
+    console.error
+  );
+
   const renderList = ({ item, index, section, separators }) => {
     return (
       <View style={styles.taskContainer}>
@@ -57,10 +84,10 @@ export default ({ navigation }) => {
           title={item.subject}
           left={(props) => <Checkbox {...props} status={item.completed ? "checked" : "unchecked"} />}
           right={(props) => 
-            <IconButton {...props} icon="close" onPress={() => {}} />
+            <IconButton {...props} icon="close" onPress={() => handleDeleteTask(item.id)} />
           }
           titleStyle={item.completed ? styles.completedTaskSubject : {}}
-          onPress={() => {}}
+          onPress={() => item.completed ? handleUncompleteTask(item.id) : handleCompleteTask(item.id)}
         />
       </View>
     )
@@ -69,7 +96,7 @@ export default ({ navigation }) => {
   return (
     <Screen>
       <SectionList
-        sections={lists}
+        sections={sectionedTasks}
         keyExtractor={(item, index) => item + index}
         renderItem={renderList}
         renderSectionHeader={({ section: { title }}) => <List.Subheader>{title} tasks</List.Subheader>}
@@ -80,10 +107,11 @@ export default ({ navigation }) => {
               <IconButton icon="plus" color="#6200ee" style={{ margin: 0 }} />
 
               <TextInput
+                ref={newTaskRef}
                 style={styles.textInput}
-                placeholder="Doing something today?"
+                placeholder={`Doing something, ${Authentication.getCurrentUserName()}?`}
                 onChangeText={setNewTask}
-                onSubmitEditing={() => {}}
+                onSubmitEditing={handleCreateTask}
               />
 
               <Menu
